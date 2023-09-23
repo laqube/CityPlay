cd4603db2ad23c136f946d7a62ecdc189f94ebbd
Miras <91594617+laqube@users.noreply.github.com>
2023-09-24 02:38:25 +0600

    feat | photobooth ineractive
    -camera connected
    -photo gets displayed below
    -frame is framing
    
    --could not do image processing, so frame is shit
    --could not do vertical photo, camera gets squished
    --design is shit, pick your colors I am a straight man not a designer

---
+++ photobooth.js
@@ -0,0 +1,42 @@
+const video = document.getElementById('camera');
+const cheeseBtn = document.getElementById('cheese-btn');
+const snapshot = document.getElementById('snapshot');
+
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+        video.srcObject = stream;
+    })
+    .catch(err => {
+        console.error('Error accessing the camera', err);
+    });
+
+cheeseBtn.addEventListener('click', () => {
+    const countdown = document.createElement('div');
+    countdown.style.position = 'absolute';
+    countdown.style.left = '50%';
+    countdown.style.top = '50%';
+    countdown.style.transform = 'translate(-50%, -50%)';
+    countdown.style.fontSize = '48px';
+    countdown.style.color = 'white';
+    document.getElementById('camera-container').appendChild(countdown);
+
+    let count = 3;
+    countdown.innerText = count;
+
+    const interval = setInterval(() => {
+        count--;
+        if (count === 0) {
+            clearInterval(interval);
+            countdown.remove();
+
+            const canvas = document.createElement('canvas');
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            const ctx = canvas.getContext('2d');
+            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+            snapshot.src = canvas.toDataURL('image/png');
+        } else {
+            countdown.innerText = count;
+        }
+    }, 1000);
+});
