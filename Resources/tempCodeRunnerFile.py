24067ee1b4b6a4b43056c204a6e5e2e0d97898be
rimskii <115300909+rimma-kubanova@users.noreply.github.com>
2023-09-24 12:28:53 +0600

    Delete Resources directory

--- Resources/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
- # key = cv2.waitKey(1)
-    # if key == ord('s'):
-    #     startGame = True
-    #     initialTime = time.time()
-    #     stateResult = False
--- Resources/test.py
+++
@@ -1,4 +0,0 @@
-import cv2
-
-cap = cv2.VideoCapture(0)
-print(cap.isOpened())
